d5e4a8c63c03ce27fe6b805124afc04f85436898
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-08 20:42:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   
   while counter < array.length do
-    new_array.push(array[counter]*)
+    new_array.push(array[counter] * array[counter])
   end
   new_array
 end
